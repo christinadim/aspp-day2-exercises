90475ff608d4150bff318c80811ffc48bf76e678
Christina Dimitriadi <christinedimitriadi@gmail.com>
2020-03-30 13:18:26 +0200

    fixed game so that it counts rounds, wins and loses correctly and finishes after 6 rounds

--- day2-bestpractices-1/buggy/dicegame/runner.py
+++ day2-bestpractices-1/buggy/dicegame/runner.py
@@ -15,8 +15,8 @@ class GameRunner:
     def answer(self):
         total = 0
         for die in self.dice:
-        #    total += 1
-	     total += die.value
+            #total += 1
+            total += die.value
         return total
 
     @classmethod
@@ -24,10 +24,13 @@ class GameRunner:
         # Probably counts wins or something.
         # Great variable name, 10/10.
         c = 0
+        wins = 0
+        loses = 0
         while True:
             runner = cls()
-
-            print("Round {}\n".format(runner.round))
+            c += 1
+            #print("Round {}\n".format(runner.round))
+            print("Round {}\n".format(c))
 
             for die in runner.dice:
                 print(die.show())
@@ -36,28 +39,36 @@ class GameRunner:
             guess = int(guess)
 
             if guess == runner.answer():
-                print("Congrats, you can add like a 5 year old...")
-                runner.wins += 1
-                c += 1
+                #print("Congrats, you can add like a 5 year old...")
+                print("Congrats, your answer is correct")
+                #runner.wins += 1
+                wins += 1
+                #c += 1
             else:
                 print("Sorry that's wrong")
                 print("The answer is: {}".format(runner.answer()))
-                print("Like seriously, how could you mess that up")
-                runner.loses += 1
-                c = 0
-            print("Wins: {} Loses {}".format(runner.wins, runner.loses))
-            runner.round += 1
+                #print("Like seriously, how could you mess that up")
+                #runner.loses += 1
+                loses += 1
+                #c = 0
+            #print("Wins: {} Loses {}".format(runner.wins, runner.loses))
+            print("Wins: {} Loses {}".format(wins, loses))
+            #runner.round += 1
 
             if c == 6:
-                print("You won... Congrats...")
-                print("The fact it took you so long is pretty sad")
+                if wins == 6:
+                    print("You won... Congrats...")
+                else:
+                    print("The game is over")
+                #print("The fact it took you so long is pretty sad")
                 break
 
             prompt = input("Would you like to play again?[y/n]: ")
 
             if prompt == 'y':
                 continue
-	    if prompt == 'n':
-		break
+            if prompt == 'n':
+                print("Thank you for playing.")
+                break
             else:
                 i_just_throw_an_exception()
